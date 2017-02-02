81fca539cb33766a032989167b63951c74f79a87
Valentin Pichard <its.valentin.pichard@gmail.com>
2017-02-02 12:23:25 +0100

    fix norm
    Signed-off-by: Valentin Pichard <its.valentin.pichard@gmail.com>

--- malloc.c
+++ malloc.c
@@ -21,9 +21,9 @@ t_chunk *g_first_chunk = NULL;
 ** Set all the meta of the chunk
 ** next is always set to NULL
 */
-static void *set_chunk(void *ptr, t_chunk chunk_data)
+static void        *set_chunk(void *ptr, t_chunk chunk_data)
 {
-  t_chunk *chunk;
+  t_chunk          *chunk;
 
   chunk = (t_chunk*) ptr;
   chunk->size = chunk_data.size;
@@ -34,37 +34,37 @@ static void *set_chunk(void *ptr, t_chunk chunk_data)
     chunk->prev->next = chunk;
   if (chunk->next)
     chunk->next->prev = chunk;
-  return ((char*) ptr) + sizeof(t_chunk) + chunk->size;
+  return ((char*)ptr + METADATA_SIZE + chunk->size);
 }
 
-static t_chunk *alloc_chunk(t_chunk *chunk, size_t size) {
-  t_chunk *next;
-  t_chunk *leftover;
-  size_t leftover_size;
+static t_chunk     *alloc_chunk(t_chunk *chunk, size_t size) {
+  t_chunk          *next;
+  t_chunk          *leftover;
+  size_t           leftover_size;
 
   if (chunk->size <= (size + METADATA_SIZE * 2 + 8))
   {
     chunk->free = 0;
-    return chunk;
+    return (chunk);
   }
   next = chunk->next;
   leftover_size = chunk->size - size - (METADATA_SIZE * 2);
 
   chunk->size = size;
   chunk->free = 0;
-  leftover = (t_chunk*) (((char*)(chunk + 1)) + size);
+  leftover = (t_chunk *)(((char*)(chunk + 1)) + size);
   chunk->next = leftover;
 
   set_chunk(leftover, (t_chunk){ leftover_size, 1, chunk, next });
 
-  return chunk;
+  return (chunk);
 }
 
-void        *malloc(size_t size)
+void               *malloc(size_t size)
 {
-  size_t  size_to_add;
-  t_chunk   *chunk;
-  t_chunk   *last_chunk;
+  size_t           size_to_add;
+  t_chunk          *chunk;
+  t_chunk          *last_chunk;
 
   size = ROUND_CHUNK_SIZE(size);
   chunk = g_first_chunk;
@@ -79,7 +79,7 @@ void        *malloc(size_t size)
   size_to_add = ROUND_HEAP_SIZE(size + METADATA_SIZE * 2 + 8);
   chunk = (t_chunk*) sbrk((intptr_t)size_to_add);
   if (chunk == (void*) -1)
-    return NULL;
+    return (NULL);
 
   last_chunk = (t_chunk*) set_chunk(chunk, ((t_chunk){ size, 0, last_chunk, NULL }));
   set_chunk(last_chunk,
